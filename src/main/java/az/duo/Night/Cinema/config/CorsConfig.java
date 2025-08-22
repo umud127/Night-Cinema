c44408fe054e936e3ff68f15d4fb5adbfd208d68
umud <umudshahmarov@gmail.com>
2025-08-22 18:31:56 +0400

    cors problem trying 4

--- src/main/java/az/duo/Night/Cinema/config/CorsConfig.java
+++ src/main/java/az/duo/Night/Cinema/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         // inkişaf mərhələsində
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://127.0.0.1:3000")
                         // prod üçün frontend host yazacaqsan, məsələn:
                         // .allowedOrigins("https://my-frontend.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
