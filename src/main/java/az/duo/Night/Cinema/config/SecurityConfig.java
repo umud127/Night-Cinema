7a4ce5467f311f06c24caf1d13adb60ad288caf1
umud <umudshahmarov@gmail.com>
2025-08-22 18:27:15 +0400

    cors problem fixing(trying 3)

--- src/main/java/az/duo/Night/Cinema/config/SecurityConfig.java
+++ src/main/java/az/duo/Night/Cinema/config/SecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @RequiredArgsConstructor
 @EnableMethodSecurity
@@ -36,7 +38,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(cors -> {})
+                .cors(withDefaults())
                 .authorizeHttpRequests(
                         authorizeRequests ->
                                 authorizeRequests
