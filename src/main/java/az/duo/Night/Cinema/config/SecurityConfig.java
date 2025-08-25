2229a547f83ddbb10a19c2bc6612169c79a5aa85
umud <umudshahmarov@gmail.com>
2025-08-25 16:34:40 +0400

    ROLE PREFIX FIXING TRYING 1

--- src/main/java/az/duo/Night/Cinema/config/SecurityConfig.java
+++ src/main/java/az/duo/Night/Cinema/config/SecurityConfig.java
@@ -29,10 +29,10 @@ public class SecurityConfig {
     private final AuthenticationProvider authProvider;
     private final JWTAuthenticationFilter jwtAuthenticationFilter;
 
-    @Bean
-    public GrantedAuthorityDefaults grantedAuthorityDefaults() {
-        return new GrantedAuthorityDefaults(""); // "ROLE_" prefixini silir (user admin yazıram Role_admin ve ya Role_user yazmiram)
-    }
+//    @Bean
+//    public GrantedAuthorityDefaults grantedAuthorityDefaults() {
+//        return new GrantedAuthorityDefaults(""); // "ROLE_" prefixini silir (user admin yazıram Role_admin ve ya Role_user yazmiram)
+//    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
