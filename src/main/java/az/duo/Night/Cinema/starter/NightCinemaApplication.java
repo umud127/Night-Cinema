337371362f19b3dd08f6be6a3b3fbbfe9b0f4190
umud <umudshahmarov@gmail.com>
2025-10-06 14:14:11 +0400

    movie controller and dto movie 2 were added or fixed

--- src/main/java/az/duo/Night/Cinema/starter/NightCinemaApplication.java
+++ src/main/java/az/duo/Night/Cinema/starter/NightCinemaApplication.java
@@ -19,12 +19,12 @@ public class NightCinemaApplication {
 
 	public static void main(String[] args) throws SQLException {
 
-        Dotenv dotenv = Dotenv.load();
-
-        // System environment-ə set et ki, Spring `${}` ilə oxuya bilsin
-        dotenv.entries().forEach(entry ->
-                System.setProperty(entry.getKey(), entry.getValue())
-        );
+//        Dotenv dotenv = Dotenv.load();
+//
+//        // System environment-ə set et ki, Spring `${}` ilə oxuya bilsin
+//        dotenv.entries().forEach(entry ->
+//                System.setProperty(entry.getKey(), entry.getValue())
+//        );
 
 		SpringApplication.run(NightCinemaApplication.class, args);
 	}
