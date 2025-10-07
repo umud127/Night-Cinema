b384ad65fb006218bd2ce8706083c6ebd306b5d4
umud <umudshahmarov@gmail.com>
2025-10-07 21:37:54 +0400

    cors problem was made

--- src/main/java/az/duo/Night/Cinema/config/CorsConfig.java
+++ src/main/java/az/duo/Night/Cinema/config/CorsConfig.java
@@ -1,24 +1,24 @@
-package az.duo.Night.Cinema.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        // inkişaf mərhələsində
-                        .allowedOrigins("https://nightcinema-6c5810225127.herokuapp.com", "http://localhost:5000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
-}
+//package az.duo.Night.Cinema.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class CorsConfig {
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        // inkişaf mərhələsində
+//                        .allowedOrigins("") //"http://localhost:5000" //https://nightcinema-6c5810225127.herokuapp.com
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                        .allowedHeaders("*")
+//                        .allowCredentials(true);
+//            }
+//        };
+//    }
+//}
