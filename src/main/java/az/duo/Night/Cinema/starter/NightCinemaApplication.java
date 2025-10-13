0f871afe66e44153f6c6c61d659c63990d20d553
umud <umudshahmarov@gmail.com>
2025-10-13 13:30:17 +0400

    phone number changed to e164 standarts. new column was added.

--- src/main/java/az/duo/Night/Cinema/starter/NightCinemaApplication.java
+++ src/main/java/az/duo/Night/Cinema/starter/NightCinemaApplication.java
@@ -19,7 +19,7 @@ public class NightCinemaApplication {
 
 	public static void main(String[] args) throws SQLException {
 
-        Dotenv dotenv = Dotenv.load();
+//        Dotenv dotenv = Dotenv.load();
 
 //        // System environment-ə set et ki, Spring `${}` ilə oxuya bilsin
 //        dotenv.entries().forEach(entry ->
