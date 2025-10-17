78cca9fdc520abb667b6a01a2d291401f6da49a2
umud <umudshahmarov@gmail.com>
2025-10-17 21:53:38 +0400

    fix try 1

--- src/main/java/az/duo/Night/Cinema/starter/NightCinemaApplication.java
+++ src/main/java/az/duo/Night/Cinema/starter/NightCinemaApplication.java
@@ -1,6 +1,5 @@
 package az.duo.Night.Cinema.starter;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
