390638d4dea617ae4359eaee34714a1be9e8e010
umud <umudshahmarov@gmail.com>
2025-10-20 18:58:46 +0400

    checking the diff for swagger

--- src/main/java/az/duo/Night/Cinema/starter/NightCinemaApplication.java
+++ src/main/java/az/duo/Night/Cinema/starter/NightCinemaApplication.java
@@ -1,5 +1,6 @@
 package az.duo.Night.Cinema.starter;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -20,12 +21,12 @@ public class NightCinemaApplication {
 
 	public static void main(String[] args) throws SQLException {
 
-//        Dotenv dotenv = Dotenv.load();
-//
-//        // System environment-ə set et ki, Spring `${}` ilə oxuya bilsin
-//        dotenv.entries().forEach(entry ->
-//                System.setProperty(entry.getKey(), entry.getValue())
-//        );
+        Dotenv dotenv = Dotenv.load();
+
+        // System environment-ə set et ki, Spring `${}` ilə oxuya bilsin
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
 
 		SpringApplication.run(NightCinemaApplication.class, args);
 	}
