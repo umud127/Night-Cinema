d9857280c64a4c4e7adf295bf33128c9091f8f08
umud <umudshahmarov@gmail.com>
2025-10-20 19:09:15 +0400

    nothing to explain

--- src/main/java/az/duo/Night/Cinema/starter/NightCinemaApplication.java
+++ src/main/java/az/duo/Night/Cinema/starter/NightCinemaApplication.java
@@ -1,5 +1,6 @@
 package az.duo.Night.Cinema.starter;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
