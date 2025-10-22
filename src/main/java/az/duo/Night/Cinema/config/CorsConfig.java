9d9663f17709651327b69381ad3f8201adc6c8b9
umud <umudshahmarov@gmail.com>
2025-10-22 21:38:42 +0400

    cors cofig was changed and mobile problem fixed

--- src/main/java/az/duo/Night/Cinema/config/CorsConfig.java
+++ src/main/java/az/duo/Night/Cinema/config/CorsConfig.java
@@ -14,7 +14,10 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         // inkişaf mərhələsində
-                        .allowedOrigins("https://nightcinema-6c5810225127.herokuapp.com", "http://localhost:5000" , "https://night-cinema.vercel.app")
+                        .allowedOrigins("https://nightcinema-6c5810225127.herokuapp.com",
+                                "http://localhost:5000" ,
+                                "http://192.168.101.3:5000" ,
+                                "https://night-cinema.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
