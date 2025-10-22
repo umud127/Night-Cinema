dccfb1aaef9f15d9cc3f552d07dd8bf633f14946
umud <umudshahmarov@gmail.com>
2025-10-22 23:15:27 +0400

    permission was created and some admin process on progress

--- src/main/java/az/duo/Night/Cinema/controller/IRestAdminController.java
+++ src/main/java/az/duo/Night/Cinema/controller/IRestAdminController.java
@@ -3,7 +3,11 @@ package az.duo.Night.Cinema.controller;
 import az.duo.Night.Cinema.dto.admin.AdminMovieDTO;
 import az.duo.Night.Cinema.dto.admin.AdminMovieSessionDTO;
 import az.duo.Night.Cinema.dto.admin.AdminUserDTO;
+import az.duo.Night.Cinema.dto.admin.ChangePermissionRequest;
 import az.duo.Night.Cinema.entity.BaseEntity;
+import az.duo.Night.Cinema.entity.Permission;
+
+import java.util.List;
 
 public interface IRestAdminController {
 
@@ -26,4 +30,7 @@ public interface IRestAdminController {
     BaseEntity<String> deleteSession(String token, Long id);
 
     BaseEntity<String> updateSession(String token, AdminMovieSessionDTO session);
+
+
+    BaseEntity<String> changePermission(String token, ChangePermissionRequest request);
 }
