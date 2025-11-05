10c3412d141d917979d70f7b9653b916b34e8914
umud <umudshahmarov@gmail.com>
2025-11-05 21:37:36 +0400

    small bug fixed

--- src/main/java/az/duo/Night/Cinema/dto/movie/DTOMovie3.java
+++ src/main/java/az/duo/Night/Cinema/dto/movie/DTOMovie3.java
@@ -1,18 +1,23 @@
 package az.duo.Night.Cinema.dto.movie;
 
+import az.duo.Night.Cinema.enums.Genre;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class DTOMovie3 {
 
+
     private String name;
     private String description;
     private String coverPhotoUrl;
-    private String movieDuration;
-    private String releaseDate;
-    private String genres;
+    private List<Genre> genres;
+    private LocalDate releaseDate;
+    private Long movieDuration;
 }
