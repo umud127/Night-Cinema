ad171ffe7de3eec834b61d50f60bfa17dfa92e23
umud <umudshahmarov@gmail.com>
2025-12-03 18:36:32 +0400

    get permission was checked by id and i changed to username

--- src/main/java/az/duo/Night/Cinema/repository/RestUserRepo.java
+++ src/main/java/az/duo/Night/Cinema/repository/RestUserRepo.java
@@ -53,6 +53,6 @@ public interface RestUserRepo extends JpaRepository<User, Long> {
     @Query("SELECT u FROM User u WHERE u.role = az.duo.Night.Cinema.enums.RoleName.USER")
     List<User> findAllUsers();
 
-    @Query("SELECT u.adminPermissions FROM User u WHERE u.id = :username")
+    @Query("SELECT u.adminPermissions FROM User u WHERE u.username = :username")
     List<Permission> findAdminPermissions(@Param("username") String username);
 }
