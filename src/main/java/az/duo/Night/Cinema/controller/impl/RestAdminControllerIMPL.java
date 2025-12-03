2f5f67c85cfb9e33e5a3ccf02d501b1a1882a4b4
umud <umudshahmarov@gmail.com>
2025-12-03 17:47:18 +0400

    get permission was checked by id and i changed to username

--- src/main/java/az/duo/Night/Cinema/controller/impl/RestAdminControllerIMPL.java
+++ src/main/java/az/duo/Night/Cinema/controller/impl/RestAdminControllerIMPL.java
@@ -100,15 +100,15 @@ public class RestAdminControllerIMPL implements IRestAdminController {
     @Override
     @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/updateSession")
-    public BaseEntity<String> updateSession(AdminMovieSessionDTO session) {
+    public BaseEntity<String> updateSession(@RequestBody AdminMovieSessionDTO session) {
         return restAdminService.updateSession( session);
     }
 
     @Override
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/getPermissions")
-    public BaseEntity<List<Permission>> getPermissions(@RequestHeader("Authorization")String token) {
-        return restAdminService.getPermissions(token.substring(7));
+    public BaseEntity<List<Permission>> getPermissions(String username) {
+        return restAdminService.getPermissions(username.substring(7));
     }
 
     @Override
